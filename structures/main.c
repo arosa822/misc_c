5c6c0469c656208949beb8fba532961a86505d82
arosa822 <h.rosa822@gmail.com>
2019-07-17 15:20:38 -0600

    adding to structure example

--- structures/main.c
+++ structures/main.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  main.c
  *
- *    Description:  playing around with structures
+ *    Description:  accessing structure members using pointer
  *
  *        Version:  1.0
  *        Created:  07/16/2019 
@@ -16,4 +16,38 @@
  * =====================================================================================
  */
 
-#include<stdio.c.>
+#include<stdio.h>
+
+struct person
+{
+    int age;
+    float weight;
+};
+
+int main()
+{
+    struct person *personPtr, person1;
+    /* assign the address of person1 to personPtr */
+    personPtr = &person1;
+
+    printf("Enter age:");
+    scanf("%d", &personPtr->age);
+
+    printf("Enter weight:");
+    scanf("%f",&personPtr->weight);
+
+    /* different methods to access structure */
+    printf("Displaying:\n");
+    printf("Age: %d\n",person1.age);
+    printf("Weight: %f\n",person1.weight);
+
+    printf("Displaying:\n");
+    printf("Age: %d\n",personPtr->age);
+    printf("Weight: %f\n",personPtr-> weight);
+
+
+
+
+
+    return 0; 
+}
