3efdc46d48b6ca3ae658aba76964578194fa940e
arosa822 <h.rosa822@gmail.com>
2019-07-24 10:47:08 -0600

    add vector example

--- dynamicArray/main.c
+++ dynamicArray/main.c
@@ -23,13 +23,23 @@
 void push(int *arr, int index, int value, int *size, int *capacity)
 {
     if(*size > *capacity){
-        printf("increasing the size of the array");
-        realloc(arr, sizeof(arr) * 2);
+        if ((realloc(arr,sizeof(arr) * 2) != NULL))
+                printf("Size of array successfully increassed..\n");
+        else 
+            printf("Error allocating memory");
+
         *capacity = sizeof(arr) * 2;
     }
 
     arr[index] = value;
     *size = *size + 1;
+}
+
+void printArray(int *arr, int size)
+{
+    int i;
+    for(i=0; i<size; i++)
+        printf(" %d ", arr[i]);
 }
 
 int main()
@@ -42,6 +52,15 @@ int main()
     push(arr,1,1, &size, &capacity);
     push(arr,2,3, &size, &capacity);
     push(arr,3,5, &size, &capacity);
+
+    printf("\n size of int: %lu\n",sizeof(int));
+
+    printf("\n size of array: %lu\n",sizeof(arr));
+
+    printf("%d\n", arr[1]);
+
+    printArray(arr,size);
+
     
 
     return 0;
